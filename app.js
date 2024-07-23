962ebb64dbddda7e0879e96f29db9f90850e3b4c
Ivanoshka <19030188@itcelaya.edu.mx>
2024-07-23 00:18:42 -0600

    se añadieron estilos

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 const API_KEY = ''
 
-async function getCompletion(){
-   const res = await fetch('https://api.openai.com/v1/completions',{
+async function getCompletion(prompt){
+   const res = await fetch('https://api.openai.com/v1/chat/completions',{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer ' + API_KEY
              },
              body: JSON.stringify({
-                model: 'gpt-3.5-turbo-instruct-0914',
-                prompt: 'DIME HOLA',
-                max_tokens: 15,
+                model: "gpt-3.5-turbo",
+                messages: [
+                    {
+                      "role": "user",
+                      "content": prompt
+                    }
+                  ]
+                
             })
 
     })
 
-    const data = await res.json()
-    console.log(data)
+    return await res.json()
+   
 }
 
-getCompletion() 
+//comunicamos el html con el css
+const prompt = document.getElementById('prompt')
+const button = document.getElementById('generate')
+const output = document.getElementById('output')
 
-
+button.addEventListener('click', async () => {
+  if (!prompt.value) {
+    return 
+  }else{
+    const response = await getCompletion(prompt.value)
+    console.log(response)
+  }
+  
+})
